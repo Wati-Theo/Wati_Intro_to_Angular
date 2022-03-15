cc88dffcb9789c1127bb5e006c20303820f6def7
Wati-Theo <watitheo@wati-popomen.home>
2022-03-15 23:53:56 +0100

    VDO10

--- Angular00/mon-app/src/app/app.component.ts
+++ Angular00/mon-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+  `,
+  styleUrls:[]
 })
+
 export class AppComponent {
-  title = 'ma Wati-WebApp';
+  title: string = "Wati-WebApp";
 }
